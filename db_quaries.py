0a3cc278f3aae2a12b938f675d9a72a864ff82ce
StefanoF66 <90260887+StefanoF66@users.noreply.github.com>
2025-03-19 14:23:51 +0100

    Delete db_quaries.py

--- db_quaries.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-from itertools import chain, combinations
-from utils import get_generators, print_generators, h1_condition_getter_ternary, get_all_equations
-df = pd.read_csv('databases/structures_simple.csv', sep=',')
-print('a')
